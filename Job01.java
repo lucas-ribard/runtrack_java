f9dbf6729b84986a79e65833ef2b3384d52445c3
lucas-ribard <lucas.ribard@laplateforme.io>
2023-09-06 10:28:14 +0200

    Job01 Complété

---
+++ Job01.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Job01 {
+    public static void main(String[] args) {
+        System.out.println("Java HelloWorld");
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Veillez saisit votre nom : ");
+        String Nom= sc.nextLine();
+        System.out.print("Hello "+Nom+"!");
+    }
+}
