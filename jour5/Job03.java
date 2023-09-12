50e26d211cfbcc501ed8facd5c943269a138087f
lucas-ribard <lucas.ribard@laplateforme.io>
2023-09-12 14:25:42 +0200

    Fix : added pair verification

--- jour5/Job03.java
+++ jour5/Job03.java
@@ -5,6 +5,11 @@ public class Job03 {
     public static void main(String[] args) {
         HashMap<String, String> hash = new HashMap<>();
 
+        if (args.length % 2 != 0) {
+            System.out.println("Le nombre d'arguments doit être pair !");
+            return;
+        }
+
         for (int i = 0; i < args.length; i += 2) {
             String cle = args[i];
             String valeur = args[i + 1];
